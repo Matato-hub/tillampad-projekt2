ca444b9bee9b5ac9c2da36cea9841a5fa5b1b3fc
Matato-hub <mathea.frost@elev.ga.ntig.se>
2026-03-16 11:09:24 +0100

    Vi fixade mediapipe HAHAHHA

--- hand.py
+++ hand.py
@@ -1,2 +1,25 @@
 
+import cv2 as cv
+
+
+# reading img
+#img = cv.imread('images/hasse-haj.png')
+
+#cv.imshow('Hasse Haj', img)
+
+# reading videos
+
+capture = cv.VideoCapture(0)
+
+while True:
+    isTrue, frame = capture.read()
+    cv.imshow('webbcam', frame)
+
+    if cv.waitKey(20) & 0xFF==ord('d'):
+        break
+
+capture.release()
+cv.destroyAllWindows
+
+#cv.waitKey(0)
 
