554ce4c392833124fbdf547473f6d3325389aca3
Matato-hub <mathea.frost@elev.ga.ntig.se>
2026-03-30 10:01:55 +0200

    lektion som ger resultat hoppas vi

--- hand.py
+++ hand.py
@@ -6,15 +6,18 @@ mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_hands = mp.solutions.hands   #ändrade från videon (där hette det mphands)
 
-string_hands = str(mp_hands)
-#wCam, hCam = 190, 380d
-with open("hands.txt", "w", encoding="utf-8") as f:  #funkar inte, fixa nästa lektion
-    f.write(string_hands)
-
 capture = cv2.VideoCapture(0)
 hands = mp_hands.Hands()    #mphands
+
+
+
+#wCam, hCam = 190, 380d
+
+
+
 while True: 
     data, image = capture.read()
+    h, w, c = image.shape
     #FLIP THE image
     image = cv2.cvtColor(cv2.flip(image, 1),cv2.COLOR_BGR2RGB)
     #storing the results
@@ -25,6 +28,13 @@ while True:
             mp_drawing.draw_landmarks(
                 image,
                 hand_landmarks, mp_hands.HAND_CONNECTIONS)
+        # Loopa igenom varje enskild punkt (0-21) i handen
+            for id, lm in enumerate(hand_landmarks.landmark):
+                # lm.x och lm.y är normaliserade (0.0 till 1.0)
+                # Här räknar vi om dem till pixlar:
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                
+                print(f"Landmark {id}: x={cx}, y={cy}")
     cv2.imshow('Handtracker', image)
     if cv2.waitKey(20) & 0xFF==ord('d'):
         break
@@ -32,3 +42,6 @@ while True:
 capture.release()
 cv2.destroyAllWindows
 
+#string_hands = str(hand_landmarks) # flyttade ner kanske inte funkar
+#    with open("hands.txt", "w", encoding="utf-8") as f:  #kanske inte funkar, flyttades ner
+#        f.write(string_hands)
